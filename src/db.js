0b2a87d29d1deb62ac5a5972956a5d2ad290a0fa
Najdorf12 <agustin.morro@gmail.com>
2024-08-27 14:39:54 -0300

    fix connectDB

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ import "dotenv/config.js";
 const MONGO = `${process.env.MONGO}`;
 export const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://agustinmorro:SFSVAXT8fozVp8uO@cluster0.oay92.mongodb.net/?retryWrites=true&w=majority&appName=Evas")
+    await mongoose.connect(MONGO)
     console.log("Conexion a mongo exitosa")
   } catch (error) {
     console.log(error);
