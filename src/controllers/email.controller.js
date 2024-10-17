481cee96387d3ccf11bc241fe620e39eee8e915d
Najdorf12 <agustin.morro@gmail.com>
2024-10-17 02:38:33 -0300

    fix resend

--- src/controllers/email.controller.js
+++ src/controllers/email.controller.js
@@ -1,16 +1,16 @@
-import { Resend } from 'resend';
+import { Resend } from "resend";
 
 // Inicializa Resend con tu API key
-const resend = new Resend('re_gcgSBD6n_NHfcokuaS34gDp7Rc4bm2yKp');  // Reemplaza con tu clave de API
+const resend = new Resend("re_gcgSBD6n_NHfcokuaS34gDp7Rc4bm2yKp"); // Reemplaza con tu clave de API
 
 // Controlador para manejar el envío de correos
 export const sendEmail = async (req, res) => {
-  const { email, wttp , message } = req.body;
+  const { email, wttp, message } = req.body;
 
   try {
     const { data, error } = await resend.emails.send({
-      from: 'TuEmpresa <info@tuempresa.com>',  // Correo remitente
-      to: ['agustin.morro@gmail.com'], // Correo destinatario
+      from: "Acme <onboarding@resend.dev>", // Correo remitente
+      to: ["agustin.morro@gmail.com"], // Correo destinatario
       subject: `Consulta de ${email}`,
       html: `
         <h1>Detalles del contacto</h1>
@@ -21,12 +21,12 @@ export const sendEmail = async (req, res) => {
     });
 
     if (error) {
-      return res.status(500).json({ error: 'Error al enviar el correo' });
+      return res.status(500).json({ error: "Error al enviar el correo" });
     }
 
-    res.status(200).json({ message: 'Correo enviado exitosamente', data });
+    res.status(200).json({ message: "Correo enviado exitosamente", data });
   } catch (err) {
-    console.error('Error en el controlador:', err);
-    res.status(500).json({ error: 'Error interno del servidor' });
+    console.error("Error en el controlador:", err);
+    res.status(500).json({ error: "Error interno del servidor" });
   }
 };
