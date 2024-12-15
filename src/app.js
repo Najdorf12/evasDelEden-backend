cb2993b48bd4ca1325316636711b007dd38d9a18
Najdorf <agustin.morro@gmail.com>
2024-12-15 19:29:12 -0300

    add compression

--- src/app.js
+++ src/app.js
@@ -2,15 +2,14 @@ import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import authRoutes from "./routes/auth.routes.js";
 import evasRoutes from "./routes/evas.routes.js";
 import emailRoutes from "./routes/email.routes.js";
+import compression from "compression";
 
 const app = express();
 app.use(
   cors({
-    /* http://localhost:5173 https://evas-del-eden-frontend.vercel.app */
     origin: [
       "http://localhost:5173",
       "https://evas-del-eden-frontend.vercel.app",
@@ -19,9 +18,9 @@ app.use(
     credentials: true,
   })
 );
-
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(compression());
 
 app.use(cookieParser());
 
